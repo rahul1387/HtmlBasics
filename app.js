60a0efd1257949ec10be23b8304c3ac32913032e
rahul1387 <rahultripathi7447@gmail.com>
2023-06-13 13:25:02 +0530

    Data saver using form tag.

---
+++ app.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector('#sub-btn');
+
+btn.addEventListener('click',function(){
+    var input1 = document.querySelector('#inpt1').value;
+    var input2 = document.querySelector('#inpt2').value;
+    if(!input1.length>0 || !input2.length>0 )
+    {
+        alert("Please enter the number")
+    }else{
+        document.querySelector('#res').innerHTML = "Result are "+(parseInt(input1)+parseInt(input2));
+        console.log("Result are "+(parseInt(input1)+parseInt(input2)));
+    }
+    
+})
